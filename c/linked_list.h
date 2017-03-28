5f4c245d1819fb73e8b71f8cab047a0c4023a5e1
Sinclair Target <sinclairtarget@gmail.com>
2017-03-27 20:20:05 -0400

    Add comments to linked list header.

--- c/linked_list.h
+++ c/linked_list.h
@@ -10,12 +10,15 @@ typedef void (*ListYieldFunc)(void* value);
 
 ListNode* ListCreate(void* value);
 
+// Returns the new list head.
 ListNode* ListPrepend(ListNode* node, void* value);
 
 ListNode* ListFind(ListNode* node, void* value);
 
+// Returns the (possibly new) list head.
 ListNode* ListRemove(ListNode* node, void* value);
 
+// Returns the new list head.
 ListNode* ListReverse(ListNode* node);
 
 void ListEach(ListNode* node, ListYieldFunc yieldFunc);
